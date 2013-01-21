00dfb5b9cf190e6d8729db970bda69843af482ae
kilimchoi <stevn1202@gmail.com>
2013-01-21 13:53:32 -0800

    adding

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,2 +1,12 @@
 class ResumeController < ApplicationController
+   def index
+      @info = Info.new
+      @info.first_name = params["first_name"] rescue nil
+      @info.last_name = params["last_name"] rescue nil
+      @info.email = params["email"] rescue nil
+      if params["commit"] == "Generate"
+         @info.save
+      end
+      redirect_to("/index/")
+   end
 end
--- app/models/info.rb
+++ app/models/info.rb
@@ -1,3 +1,13 @@
 class Info < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name
+  def user_data 
+     data = {
+	:email = self.email,
+	:first_name = self.first_name,
+	:last_name = self.last_name,
+	:address1 = self.address1,
+	:address2 = self.address2, 
+     }
+     return data
+   end	
 end
