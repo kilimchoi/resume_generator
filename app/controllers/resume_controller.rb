9c2198c72822aa6b399fc5cd902d41ab5209ed06
kilimchoi <stevn1202@gmail.com>
2013-01-21 14:22:47 -0800

    adding

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,14 +1,18 @@
 class ResumeController < ApplicationController
    def index
       @info = Info.new
-      @info.first_name = params["first_name"] rescue nil
-      @info.last_name = params["last_name"] rescue nil
-      @info.email = params["email"] rescue nil
-      @info.address1 = params["address1"] rescue nil
-      @info.address2 = params["address2"] rescue nil
+      @info.first_name = params[:first_name] rescue nil
+      @info.last_name = params[:last_name] rescue nil
+      @info.email = params[:email] rescue nil
+      @info.address1 = params[:address1] rescue nil
+      @info.address2 = params[:address2] rescue nil
       if params["commit"] == "Generate"
          @info.save
       end
       redirect_to("/index/")
    end
+   
+   def template
+     redirect_to("/template/")
+   end 
 end
