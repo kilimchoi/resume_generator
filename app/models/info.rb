40ddf59070b466a9251c41b7dbb696818a1c36b1
kilimchoi <stevn1202@gmail.com>
2013-01-21 14:32:33 -0800

    changing name

--- app/models/info.rb
+++ app/models/info.rb
@@ -1,10 +1,9 @@
 class Info < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name, :address1, :address2, :honors, :work_place, :work_description
+  attr_accessible :email, :full_name, :address1, :address2, :honors, :work_place, :work_description
   def user_data 
      data = {
 	:email = self.email,
-	:first_name = self.first_name,
-	:last_name = self.last_name,
+	:full_name = self.full_name,
 	:address1 = self.address1,
 	:address2 = self.address2,
 	:work_place = self.work_place, 
